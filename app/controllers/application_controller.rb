4d6fef4137144db6997123f67a5add23c69d7cbb
Matias <matiasrenta@gmail.com>
2015-07-12 01:34:24 -0500

    paginate en tabla things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-#require 'will_paginate/array'
-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -20,12 +18,8 @@ class ApplicationController < ActionController::Base
     set_content_title(t("screens.labels.access_denied"))
   end
 
-  def action_controller_error
-    if current_user
-      raise ActiveRecord::RecordNotFound.new("Probably a routing error")
-    else
-      redirect_to new_user_session_path
-    end
+  def raise_not_found!
+      raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
   end
 
   protected ####################################### PROTECTED ###################################################
@@ -108,8 +102,13 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
-  rescue_from Exception, :with => :render_500
-  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  #continue to use rescue_from in the same way as before
+  #unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, :with => :render_500
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+    rescue_from ActionController::RoutingError, :with => :render_404
+  #end
   #rescue_from CanCan::AccessDenied do |exception|
   #  redirect_to "/application/access_denied", :alert => t("screens.labels.access_denied")
   #end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root to: 'welcome#index'
 
-  #match '*path' => "application#action_controller_error", via: [:get, :post]
+  #match '*unmatched_route', :to => 'application#raise_not_found!', :via => :all
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
