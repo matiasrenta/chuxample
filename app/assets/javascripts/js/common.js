c8eeaa0e566ebd0839b83dfc3103e54714302184
Bernardo Kuri <github@bkuri.com>
2015-07-18 00:46:12 -0500

    responsive datatables

--- app/assets/javascripts/js/common.js
+++ app/assets/javascripts/js/common.js
@@ -1,22 +1,11 @@
 // init tables
 //$('table').DataTable({paging:false,searching:false,info:false,ordering:false});
 
-$('table').dataTable({
+$('table').DataTable({
   autoWidth: false,
   paging: false,
   searching: false,
   info: false,
   ordering: false,
-  preDrawCallback: function () {
-    // Initialize the responsive datatables helper once.
-    if (!responsiveHelper) {
-      responsiveHelper = new ResponsiveDatatablesHelper(tableElement, breakpointDefinition);
-    }
-  },
-  rowCallback    : function (nRow) {
-    responsiveHelper.createExpandIcon(nRow);
-  },
-  drawCallback   : function (oSettings) {
-    responsiveHelper.respond();
-  }
+  responsive: true
 });
