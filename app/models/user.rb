f6b79d58209de2ff241df5a4af33482fd2be7bf8
Matias <matiasrenta@gmail.com>
2015-08-03 17:19:38 -0500

    user model preparado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
+require 'valid_email'
+
 class User < ActiveRecord::Base
   belongs_to :role
   # Include default devise modules. Others available are:
   # :registerable, :confirmable, :validatable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :timeoutable, :lockable
+
+  validates :email, email: {message: I18n.t('errors.messages.invalid_email')}, mx: {message: I18n.t('errors.messages.invalid_mx')}
+
 end
