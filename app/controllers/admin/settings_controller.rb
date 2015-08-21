5f5fb160d813e54041078ddfa1266d82938fdb6a
Matias <matiasrenta@gmail.com>
2015-08-20 19:33:21 -0500

    crud settings completo

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,7 +1,39 @@
 class Admin::SettingsController < Admin::ApplicationController
   def index
-    # to get all items for render list
-    @settings = Settings.unscoped
+    authorize!(:read, Settings)
+    search_algoritm
+    if params[:q] && params[:q][:meta_sort]
+      @q = Settings.unscoped.accessible_by(current_ability, :read).ransack(params[:q])
+    else
+      @q = Settings.unscoped.order("updated_at DESC, created_at DESC").accessible_by(current_ability, :read).ransack(params[:q])
+    end
+    nodel_collection = @q.result(distinct: true)
+    @settings = nodel_collection.paginate(:page => params[:page], :per_page => per_page(params[:per_page]))
+  end
+
+  def search_algoritm
+    if params[:search_clear]
+      params[:q] = nil
+      params[:search_clear] = nil
+    end
+
+    if params[:q]
+      params[:q].each do |param|
+        unless param[1].blank? || param[0] == 's' # la 's' es para que no se ponga rojo cuando solo se hace sort de columnas
+          @filter_active = true;
+          break
+        end
+      end
+    end
+  end
+
+  def per_page(quantity)
+    if !quantity.blank?
+      params[:per_page] = quantity
+    elsif cookies[:per_page].blank?
+      params[:per_page] = 20
+    end
+    params[:per_page]
   end
 
   def new
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -35,6 +35,7 @@ class Ability
   end
 
   def superuser
+    can [:manage], Settings
 		can [:manage], User
 		can [:manage], Thing
 		can :manage, ChuckyBot
