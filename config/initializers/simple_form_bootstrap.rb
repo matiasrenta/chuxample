d6fee23379cf5ebaaa65184cd9e64377132b5082
Matias <matiasrenta@gmail.com>
2015-09-24 13:43:10 -0500

    boolean en forms

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -84,15 +84,22 @@ SimpleForm.setup do |config|
   config.wrappers :horizontal_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
+    b.use :label, class: 'col-sm-3 control-label'
 
-    b.wrapper tag: 'div', class: 'col-sm-offset-3 col-sm-9' do |wr|
-      wr.wrapper tag: 'div', class: 'checkbox' do |ba|
-        ba.use :label_input, class: 'col-sm-9'
-      end
-
-      wr.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-      wr.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+      ba.use :input, class: 'col-sm-9'
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
+
+    #b.wrapper tag: 'div', class: 'col-sm-offset-3 col-sm-9' do |wr|
+    #  wr.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+    #    ba.use :input, class: 'col-sm-9'
+    #  end
+    #
+    #  wr.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+    #  wr.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    #end
   end
 
   config.wrappers :horizontal_radio_and_checkboxes, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
