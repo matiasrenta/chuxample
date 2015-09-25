4887164c2f35e511a9fdbfee3b3f6e529f5fe5c8
Matias <matiasrenta@gmail.com>
2015-09-24 20:17:09 -0500

    wrapper for file table form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -51,6 +51,10 @@ SimpleForm.setup do |config|
     # b.use :full_error, wrap_with: { tag: :span, class: :error }
   end
 
+  config.wrappers :clean do |b|
+    b.use :input
+  end
+
   # The default wrapper to be used by the FormBuilder.
   config.default_wrapper = :default
 
