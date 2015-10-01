563376c1e271be731168f8dec0ac1438bf8c97a1
Matias <matiasrenta@gmail.com>
2015-10-01 14:01:30 -0500

    welcome prueba

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # Rout para probar paginas
+  get 'welcome/prueba'
+
   # You can have the root of your site routed with "root"
   root to: 'welcome#index'
 
