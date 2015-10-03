3cd6039e261479b9740c1ee8c603da42262c38bd
Matias <matiasrenta@gmail.com>
2015-10-02 19:04:13 -0500

    theme in public act

--- app/controllers/public_activity_controller.rb
+++ app/controllers/public_activity_controller.rb
@@ -4,6 +4,6 @@ class PublicActivityController < ApplicationController
   def index
     authorize!(:read, PublicActivity::Activity)
     set_content_title('fa-fw fa fa-video-camera', ['Public Activity'])
-    @activities = PublicActivity::Activity.first(10)
+    @activities = PublicActivity::Activity.all
   end
 end
