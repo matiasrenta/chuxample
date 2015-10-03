a0b269ca7284a7baa56c0be7e570fef920f1f535
Matias <matiasrenta@gmail.com>
2015-10-02 19:15:52 -0500

    paginacion en public act

--- app/controllers/public_activity_controller.rb
+++ app/controllers/public_activity_controller.rb
@@ -4,6 +4,9 @@ class PublicActivityController < ApplicationController
   def index
     authorize!(:read, PublicActivity::Activity)
     set_content_title('fa-fw fa fa-video-camera', ['Public Activity'])
-    @activities = PublicActivity::Activity.all
+    per_page = params[:per_page].blank? ? 10 : params[:per_page].to_i
+    @q = PublicActivity::Activity.order('id desc').ransack(params[:q])
+    public_activities = @q.result(distinct: true)
+    @activities = public_activities.paginate(:page => params[:page].blank? ? 1 : params[:page], :per_page => per_page)
   end
 end
