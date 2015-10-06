90de9019d0c6386c2bf758f88a0065a33d238beb
Matias <matiasrenta@gmail.com>
2015-10-05 20:45:17 -0500

    public act filtros completos

--- app/helpers/public_activity_helper.rb
+++ app/helpers/public_activity_helper.rb
@@ -6,4 +6,19 @@ module PublicActivityHelper
   rescue => error
     return false
   end
+
+  def trackables
+    all_trackables = PublicActivity::Activity.select(:trackable_type).group(:trackable_type).order(:trackable_type).map(&:trackable_type)
+    existing_trackables = Array.new
+    all_trackables.each {|t| existing_trackables << t if exists_class?(t)}
+    opt = Array.new
+    existing_trackables.each do |t|
+      opt << OpenStruct.new(id: t, name: t("activerecord.models.#{t.underscore}.one"))
+    end
+    opt
+  end
+
+  def keyables
+    [['Creó', 'create'], ['Actualizó', 'update'], ['Borró', 'destroy']]
+  end
 end
