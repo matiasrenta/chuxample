a76c16e9618e2d7ecefb99480cff3920343a294b
Matias <matiasrenta@gmail.com>
2015-10-06 17:39:50 -0500

    public act update no recording when no chenges made

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -3,6 +3,7 @@ class Thing < ActiveRecord::Base
 	tracked only: [:create, :update, :destroy]
 	tracked owner: ->(controller, model) {controller.try(:current_user)}
 	#tracked recipient: ->(controller, model) { model.xxxx }
+	tracked :on => {:update => proc {|model, controller| model.changes.except(*model.except_attr_in_public_activity).keys.size > 0 }}
   tracked :parameters => {
 							:attributes_changed => proc {|controller, model| model.id_changed? ? nil : model.changes.except(*model.except_attr_in_public_activity)},
 							:model_label => proc {|controller, model| model.try(:name)}
--- lib/generators/chucky_scaff/chucky_scaff_generator.rb
+++ lib/generators/chucky_scaff/chucky_scaff_generator.rb
@@ -111,7 +111,9 @@ class ChuckyScaffGenerator < Rails::Generators::NamedBase
         "\tinclude PublicActivity::Model
   #{tracked}
   tracked :on => {update: proc {|model, controller| model.changes.except(*model.except_attr_in_public_activity).size > 0 }}
-  tracked owner: ->(controller, model) {controller.try(:current_user)}\n\t#tracked recipient: ->(controller, model) { model.xxxx }
+  tracked owner: ->(controller, model) {controller.try(:current_user)}
+  #tracked recipient: ->(controller, model) { model.xxxx }
+  tracked :on => {:update => proc {|model, controller| model.changes.except(*model.except_attr_in_public_activity).keys.size > 0 }}
   tracked :params => {
               :attributes_changed => proc {|controller, model| model.id_changed? ? nil : model.changes.except(*model.except_attr_in_public_activity)},
               :model_label => proc {|controller, model| model.try(:name)}
