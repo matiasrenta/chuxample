dc854a5a11d1152cb7f0c7b5999fb49d966e9925
Matias <matiasrenta@gmail.com>
2015-10-19 13:05:13 -0500

    nada

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,22 +40,22 @@ namespace :deploy do
     Rake::Task['deploy:assets:precompile'].clear_actions
     desc 'Precompile assets locally and upload to servers'
     task :precompile do
-      on roles(fetch(:assets_roles)) do
-        run_locally do
-          with rails_env: fetch(:rails_env) do
-            execute 'bin/rake assets:precompile'
-          end
-        end
-        within release_path do
-          with rails_env: fetch(:rails_env) do
-            old_manifest_path = "#{shared_path}/public/assets/manifest*"
-            execute :rm, old_manifest_path if test "[ -f #{old_manifest_path} ]"
-            #upload!('./public/assets/', "#{shared_path}/public/", recursive: true)
-            upload!('./public/assets/', "#{shared_path}/public/", recursive: true)
-          end
-        end
-        run_locally { execute 'rm -rf public/assets' }
-      end
+      #on roles(fetch(:assets_roles)) do
+      #  run_locally do
+      #    with rails_env: fetch(:rails_env) do
+      #      execute 'bin/rake assets:precompile'
+      #    end
+      #  end
+      #  within release_path do
+      #    with rails_env: fetch(:rails_env) do
+      #      old_manifest_path = "#{shared_path}/public/assets/manifest*"
+      #      execute :rm, old_manifest_path if test "[ -f #{old_manifest_path} ]"
+      #      #upload!('./public/assets/', "#{shared_path}/public/", recursive: true)
+      #      upload!('./public/assets/', "#{shared_path}/public/", recursive: true)
+      #    end
+      #  end
+      #  run_locally { execute 'rm -rf public/assets' }
+      #end
     end
   end
 
