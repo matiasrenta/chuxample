67ce3fe8895d43099e23d32a250811d6437c2c05
esaugtz <dgesau@gmail.com>
2016-01-19 20:22:01 -0600

    funcion exportar a excel corregida

--- app/assets/javascripts/chucky_config.js
+++ app/assets/javascripts/chucky_config.js
@@ -325,7 +325,6 @@ $(function() {
 });
 
 	//Select2 Nueva Implementacion
-
 	//Carga Json de Estados ejemplo
 	var dataPartial;
 	$.getJSON("../assets/partial.json", function (data) {
@@ -333,14 +332,8 @@ $(function() {
 	});
 
 	setTimeout(function(){ console.log(dataPartial); }, 1000);
-	setTimeout(function(){ $(".selectComponent").select2({ data: dataPartial }); }, 1000);
-	setTimeout(function(){ $("#selectSimple").select2({ data: dataPartial, minimumResultsForSearch: Infinity }); }, 1000);
-	setTimeout(function(){ $("#selectMultipleAuto").select2({ data: dataPartial, maximumSelectionLength: 2 }); }, 1000);
-
-	// var json = [
-	// 	{"id":1,"text":"Aguascalientes"},
-	// 	{"id":2,"text":"Baja California"},
-	// 	{"id":3,"text":"Baja California Sur"}
-	// ];
-	//
-	// //$("#selectSingle").select2({ data: json });
+	setTimeout(function(){
+		$(".selectComponent").select2({ data: dataPartial });
+		$("#selectSimple").select2({ data: dataPartial, minimumResultsForSearch: Infinity });
+		$("#selectMultipleAuto").select2({ data: dataPartial, maximumSelectionLength: 2 });
+	}, 1000);
