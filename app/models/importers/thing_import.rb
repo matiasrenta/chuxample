b26c21f81ab9d6f3effb1a8246eca9b1656bfb00
Matias <matiasrenta@gmail.com>
2016-01-19 20:28:59 -0600

    mas columnas en import

--- app/models/importers/thing_import.rb
+++ app/models/importers/thing_import.rb
@@ -3,6 +3,8 @@ class ThingImport < BaseImport
     thing = Thing.new
     thing.name = row[I18n.translate('simple_form.labels.defaults.name')]
     thing.age = row[I18n.translate('simple_form.labels.defaults.age')]
+    thing.price = row[I18n.translate('simple_form.labels.defaults.precio')]
+    thing.expires = row[I18n.translate('simple_form.labels.defaults.expira')]
     thing
   end
 end
