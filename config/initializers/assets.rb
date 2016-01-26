0ed59c5ba2724c72ef4cc3daee562f793e659d92
Matias <matiasrenta@gmail.com>
2016-01-26 12:04:59 -0600

    quite selec2 jss del asset ini

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( js/common.js js/app.js chucky_config.js auto_select2/static_select2 auto_select2/ajax_select2 auto_select2/ajax_multi_select2)
+Rails.application.config.assets.precompile += %w( js/common.js js/app.js chucky_config.js )
