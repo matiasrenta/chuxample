8d3cc18c19c3fde66827b3345b6c60f4ce57e50f
Matias <matiasrenta@gmail.com>
2016-01-28 21:26:59 -0600

    intento assets commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   #config.assets.prefix = '/dev-assets'
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  #config.assets.compile = false
+  config.assets.compile = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
