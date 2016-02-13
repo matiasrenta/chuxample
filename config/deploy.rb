2b9980419123e23b5f536ceb3c2ba1a62e0a9846
Matias <matiasrenta@gmail.com>
2016-02-12 21:06:16 -0600

    uploads dir linked in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :bundle_binstubs, nil
 set :linked_files, %w{config/database.yml config/secrets.yml .env.production}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/assets public/system}
+set :linked_dirs, %w{uploads log tmp/pids tmp/cache tmp/sockets vendor/bundle public/assets public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
