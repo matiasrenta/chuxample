ce5940047b31d8532aadc3732b12989eef6d46ed
Matias <matiasrenta@gmail.com>
2016-04-26 17:33:16 -0500

    Actividad de Usuarios

--- app/controllers/public_activity_controller.rb
+++ app/controllers/public_activity_controller.rb
@@ -2,7 +2,7 @@ class PublicActivityController < ApplicationController
   #authorize_resource except: :index
 
   def index
-    set_content_title('fa-fw fa fa-video-camera', ['Activity Feed'])
+    set_content_title('fa-fw fa fa-video-camera', ['Actividad de Usuarios'])
     @activities = do_index(PublicActivity::Activity, params, nil, true, 'id desc')
   end
 
