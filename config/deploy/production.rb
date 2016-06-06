3eb5950f0c14a345eebb9b544e7d288bab787897
Matias <matiasrenta@gmail.com>
2016-06-06 13:32:43 -0500

    nuevo servidor para deployar

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deployer@45.55.60.93}
-role :web, %w{deployer@45.55.60.93}
-role :db,  %w{deployer@45.55.60.93}
+role :app, %w{deployer@104.236.104.0}
+role :web, %w{deployer@104.236.104.0}
+role :db,  %w{deployer@104.236.104.0}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{deployer@45.55.60.93}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '45.55.60.93', user: 'deployer', roles: %w{web app}, my_property: :my_value
+server '104.236.104.0', user: 'deployer', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
